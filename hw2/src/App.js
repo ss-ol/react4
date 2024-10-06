bf484ec0d784f523c54e775973d00359e35c6aff
ss-ol <leks37324@gmail.com>
2024-10-06 12:02:36 +0600

    Add files via upload

---
+++ hw2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MainPage from './pages/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <MainPage/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
